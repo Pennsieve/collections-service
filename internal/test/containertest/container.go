5cf33edd683ec3ea0cd487159c65e8494cc0d052
John Frommeyer <johnfr@seas.upenn.edu>
2025-03-19 17:41:54 -0400

    Document test methods

--- internal/test/containertest/container.go
+++ internal/test/containertest/container.go
@@ -7,6 +7,11 @@ import (
 	"github.com/pennsieve/collections-service/internal/test/configtest"
 )
 
+// Config returns a config.Config suitable for use against
+// the pennseivedb instance started for testing. It is preferred in tests over
+// calling config.LoadConfig() because that method
+// will not create the correct configs if the tests are running locally instead
+// of in the Docker test container.
 func Config() config.Config {
 	return config.Config{PostgresDB: configtest.PostgresDBConfig()}
 }
