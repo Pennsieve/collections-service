db31f9417e6ce642d6466ea39a4f88f039054e3a
John Frommeyer <johnfr@seas.upenn.edu>
2025-03-25 20:27:56 -0400

    Make collections owner node id not null

--- internal/dbmigrate/migrations/20250319135517_create_collections.up.sql
+++ internal/dbmigrate/migrations/20250319135517_create_collections.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS collections
     id            SERIAL PRIMARY KEY,
     name          VARCHAR(255)        NOT NULL,
     description   VARCHAR(255),
-    owner_node_id VARCHAR(255),
+    owner_node_id VARCHAR(255)        NOT NULL,
     updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     node_id       VARCHAR(255) UNIQUE NOT NULL
