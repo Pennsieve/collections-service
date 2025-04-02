daa09be59d4e026afdc3562244e500fec89a3a09
John Frommeyer <johnfr@seas.upenn.edu>
2025-04-02 15:53:57 -0400

    No RDS Proxy for dbmigrate, so we need a postgres password.

--- cmd/dbmigrate/main.go
+++ cmd/dbmigrate/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
-	awsCfg "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/pennsieve/collections-service/internal/dbmigrate"
 	"log/slog"
 	"os"
@@ -18,17 +16,20 @@ func main() {
 		logger.Error("error loading config", slog.Any("error", err))
 		os.Exit(1)
 	}
+	if migrateConfig.PostgresDB.Password == nil {
+		logger.Error("password must be specified; cannot currently use RDS proxy for migrates since no Postgres role with the appropriate grants has credentials in the proxy")
+		os.Exit(1)
+	}
 	logger.
 		With(slog.Bool("verboseLogging", migrateConfig.VerboseLogging),
 			slog.Group("postgres",
 				slog.String("host", migrateConfig.PostgresDB.Host),
 				slog.Int("port", migrateConfig.PostgresDB.Port),
 				slog.String("username", migrateConfig.PostgresDB.User),
-				slog.Bool("useRDSProxy", migrateConfig.PostgresDB.Password == nil),
 				slog.String("database", migrateConfig.PostgresDB.CollectionsDatabase),
 			)).
 		Info("collections DB schema migration started")
-	m, err := newCollectionsManager(ctx, migrateConfig)
+	m, err := dbmigrate.NewLocalCollectionsMigrator(ctx, migrateConfig)
 	if err != nil {
 		logger.Error("error creating CollectionsMigrator", slog.Any("error", err))
 		os.Exit(1)
@@ -41,15 +42,4 @@ func main() {
 	}
 
 	logger.Info("collections DB schema migration complete")
-}
-
-func newCollectionsManager(ctx context.Context, migrateConfig dbmigrate.Config) (*dbmigrate.CollectionsMigrator, error) {
-	if migrateConfig.PostgresDB.Password == nil {
-		awsConfig, err := awsCfg.LoadDefaultConfig(ctx)
-		if err != nil {
-			return nil, fmt.Errorf("error loading AWS config: %w", err)
-		}
-		return dbmigrate.NewRDSProxyCollectionsMigrator(ctx, migrateConfig, awsConfig)
-	}
-	return dbmigrate.NewLocalCollectionsMigrator(ctx, migrateConfig)
 }
