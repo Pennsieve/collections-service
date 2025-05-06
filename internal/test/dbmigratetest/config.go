38a9d6d655434bcdd1b9beb092044054feda1bac
John Frommeyer <johnfr@seas.upenn.edu>
2025-05-06 15:59:45 -0400

    Remove internal dbmigrate in favor of new dbmigrate-go module.

--- internal/test/dbmigratetest/config.go
+++ internal/test/dbmigratetest/config.go
@@ -1,18 +1,43 @@
 package dbmigratetest
 
 import (
-	"github.com/pennsieve/collections-service/internal/dbmigrate"
+	collectionsconfig "github.com/pennsieve/collections-service/internal/dbmigrate"
+	sharedconfig "github.com/pennsieve/collections-service/internal/shared/config"
 	"github.com/pennsieve/collections-service/internal/test/configtest"
+	"github.com/pennsieve/dbmigrate-go/pkg/shared/config"
 )
 
-// Config returns a dbmigrate.Config suitable for use against
+// Config returns a [config.Config] suitable for use against
 // the pennseivedb instance started for testing. It is preferred in tests over
-// calling dbmigrate.LoadConfig() because that method
+// calling [config.LoadConfig] because that method
 // will not create the correct configs if the tests are running locally instead
 // of in the Docker test container.
-func Config(pgOptions ...configtest.PostgresOption) dbmigrate.Config {
-	return dbmigrate.Config{
-		PostgresDB:     configtest.PostgresDBConfig(pgOptions...),
+func Config(pgOptions ...configtest.PostgresOption) config.Config {
+	return config.Config{
+		PostgresDB:     DBMigratePostgresDBConfig(pgOptions...),
 		VerboseLogging: true,
+	}
+}
+
+func DBMigratePostgresDBConfig(pgOptions ...configtest.PostgresOption) config.PostgresDBConfig {
+	defaults := collectionsconfig.ConfigDefaults()
+	localconfig := configtest.PostgresDBConfig(pgOptions...)
+	return config.PostgresDBConfig{
+		Host:     localconfig.Host,
+		Port:     localconfig.Port,
+		User:     localconfig.User,
+		Password: localconfig.Password,
+		Database: localconfig.CollectionsDatabase,
+		Schema:   defaults["POSTGRES_SCHEMA"],
+	}
+}
+
+func ToSharedPostgresDBConfig(config config.PostgresDBConfig) sharedconfig.PostgresDBConfig {
+	return sharedconfig.PostgresDBConfig{
+		Host:                config.Host,
+		Port:                config.Port,
+		User:                config.User,
+		Password:            config.Password,
+		CollectionsDatabase: config.Database,
 	}
 }
