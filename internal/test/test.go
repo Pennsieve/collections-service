659f9e4968b76ec2d6be6213ad67ff1af7fa6c31
John Frommeyer <johnfr@seas.upenn.edu>
2025-05-13 20:33:23 -0400

    Refactoring test

--- internal/test/test.go
+++ internal/test/test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"github.com/pennsieve/collections-service/internal/api/dto"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -10,12 +9,4 @@ func Helper(t require.TestingT) {
 	if tt, hasHelper := t.(*testing.T); hasHelper {
 		tt.Helper()
 	}
-}
-
-func GroupByDOI(datasets ...dto.PublicDataset) map[string]dto.PublicDataset {
-	byDOI := map[string]dto.PublicDataset{}
-	for _, dataset := range datasets {
-		byDOI[dataset.DOI] = dataset
-	}
-	return byDOI
 }
