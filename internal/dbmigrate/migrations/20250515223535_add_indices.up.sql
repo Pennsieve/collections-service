c3ca9dbdcf0140412c17cffe1c28ea6189df76c0
John Frommeyer <johnfr@seas.upenn.edu>
2025-05-16 16:27:20 -0400

    Update index since we've added the permission_bit check back into the query

--- internal/dbmigrate/migrations/20250515223535_add_indices.up.sql
+++ internal/dbmigrate/migrations/20250515223535_add_indices.up.sql
@@ -1,5 +1,5 @@
-CREATE INDEX collection_user_user_collection_idx
-    ON collection_user (user_id, collection_id);
+CREATE INDEX collection_user_user_perm_coll_idx
+    ON collections.collection_user (user_id, permission_bit, collection_id);
 
 CREATE INDEX IF NOT EXISTS dois_collection_id_id_idx
     ON collections.dois (collection_id, id);
