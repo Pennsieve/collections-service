07263d37cc201304837b4e24f8f03345d766aa47
John Frommeyer <johnfr@seas.upenn.edu>
2025-07-22 13:40:03 -0400

    Fix import of publish bucket name from platform infrastructure (#40)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -28,7 +28,7 @@ resource "aws_lambda_function" "collections_service_api_lambda" {
       DISCOVER_SERVICE_HOST         = local.discover_service_host,
       PENNSIEVE_DOI_PREFIX          = local.pennsieve_doi_prefix,
       COLLECTIONS_ID_SPACE_ID       = local.collections_id_space_id,
-      PUBLISH_BUCKET                = data.terraform_remote_state.platform_infrastructure.outputs.discover_publish50_bucket_name,
+      PUBLISH_BUCKET                = data.terraform_remote_state.platform_infrastructure.outputs.discover_publish50_bucket_id,
       LOG_LEVEL                     = local.log_level
     }
   }
